0f4c441d7794c71764de275ff3aa41221da87f32
Tim Oltman <toltman@gmail.com>
2020-06-12 11:59:45 -0400

    Updates

--- chart.6faa0bbc.js
+++ chart.6faa0bbc.js
@@ -2379,4 +2379,4 @@ module.exports={name:"vega-tooltip",version:"0.23.0",description:"A tooltip plug
 },{"tslib":"CvJj","fast-json-patch":"uWIx","json-stringify-pretty-compact":"viAE","semver":"b3dR","vega":"bYg9","vega-lite":"Xk5G","vega-schema-url-parser":"Crjx","vega-themes":"FE0p","vega-tooltip":"fSAG","vega-util":"xvkM","./post":"nbHf","./style":"tWOI","./util":"ZGJj","./types":"npUr"}],"YxI8":[function(require,module,exports) {
 "use strict";var a=e(require("vega-embed"));function e(a){return a&&a.__esModule?a:{default:a}}var t={$schema:"https://vega.github.io/schema/vega-lite/v4.0.json",description:"Temperature Ranges in Seattle and New York",data:{values:[{a:"A",b:28},{a:"B",b:55},{a:"C",b:43},{a:"D",b:91},{a:"E",b:81},{a:"F",b:53},{a:"G",b:19},{a:"H",b:87},{a:"I",b:52}]},mark:"bar",encoding:{x:{field:"a",type:"ordinal"},y:{field:"b",type:"quantitative"}}};(0,a.default)("#chart",t);
 },{"vega-embed":"sslh"}]},{},["YxI8"], null)
-//# sourceMappingURL=/chart.6faa0bbc.js.map
+//# sourceMappingURL=/temperature-chart/chart.6faa0bbc.js.map
